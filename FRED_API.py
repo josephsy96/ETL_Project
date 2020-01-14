4b642064970763a50543f8e2591c3e6a807711b3
josephsy96 <josephsy96@hotmail.com>
2020-01-13 20:47:34 -0600

    economic data

---
+++ FRED_API.py
@@ -0,0 +1,38 @@
+"""
+Author: Joseph Yi
+Abstract:
+Purpose of this script is to pull US Economic Data to present an interactive chart of economic data over a given time-series 
+"""
+
+#Import Dependencies 
+import os
+import pandas as pd 
+from fredapi import Fred
+from pprint import pprint 
+
+
+#Import API Key
+from config import api_key
+
+fred = Fred(api_key)
+#This is to set the directory for the script
+os.chdir('/Users/josephyi/Documents/VSCODE/FED API Data')
+
+#create parameters for requests
+param = {'':''}
+
+print('---' * 20 + '\nWelcome\n' + '---' * 20)
+eco_code = input("Please input the economic code to pull:")
+
+
+#Pull Monetary Data from the FRED API, specifically M1 Data 
+try:
+    eco = fred.get_series_all_releases(eco_code)
+    print('---' * 20)
+    pprint(eco)
+    eco_df = pd.DataFrame(eco)
+    eco_df.to_csv(f'{eco_code}.csv')
+except:
+    print("it's not working...")
+
+print("Done pulling M1 Data")
